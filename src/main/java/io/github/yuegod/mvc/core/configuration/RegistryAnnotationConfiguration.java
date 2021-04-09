9f181396d8751e1cd461901ebb5e6e010505fc37
YueGod <31380473@qq.com>
2021-04-09 16:27:50 +0800

    fix:sorted bug

--- src/main/java/io/github/yuegod/mvc/core/configuration/RegistryAnnotationConfiguration.java
+++ src/main/java/io/github/yuegod/mvc/core/configuration/RegistryAnnotationConfiguration.java
@@ -5,6 +5,7 @@ import io.github.yuegod.mvc.core.annotation.Registry;
 import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author quziwei
@@ -14,30 +15,33 @@ import java.util.Set;
 public class RegistryAnnotationConfiguration {
 
 
-    /**
-     * 获取加了Registry注解的类型,并且按Order对注册类进行排序
-     *
-     * @return 返回一个排好序的RegistryDescription集合，改类型是对Registry类的一个描述
-     */
-    public Set<RegistryDescription> getRegistryClazz(Set<Class<?>> scannedClass) {
-        Set<RegistryDescription> registryDescriptions = new LinkedHashSet<>();
-        for (Class<?> clazz : scannedClass) {
-            if (isRegistry(clazz)) {
-                Registry registry = clazz.getAnnotation(Registry.class);
-                RegistryDescription registryDescription = new RegistryDescription(clazz.getName(), clazz, registry.order());
-                registryDescriptions.add(registryDescription);
-            }
-        }
-        //按Order进行排序
-        registryDescriptions.stream().sorted(Comparator.comparingInt(RegistryDescription::getOrder));
-        return registryDescriptions;
+  /**
+   * 获取加了Registry注解的类型,并且按Order对注册类进行排序
+   *
+   * @return 返回一个排好序的RegistryDescription集合，改类型是对Registry类的一个描述
+   */
+  public Set<RegistryDescription> getRegistryClazz(Set<Class<?>> scannedClass) {
+    Set<RegistryDescription> registryDescriptions = new LinkedHashSet<>();
+    for (Class<?> clazz : scannedClass) {
+      if (isRegistry(clazz)) {
+        Registry registry = clazz.getAnnotation(Registry.class);
+        RegistryDescription registryDescription = new RegistryDescription(clazz.getName(), clazz,
+            registry.order());
+        registryDescriptions.add(registryDescription);
+      }
     }
+    //按Order进行排序
+    return registryDescriptions.stream()
+        .sorted(Comparator.comparingInt(RegistryDescription::getOrder)).collect(
+            Collectors.toCollection(LinkedHashSet::new));
 
-    /**
-     * 判断该类是否加了Registry注解
-     */
-    private boolean isRegistry(Class<?> clazz) {
-        return clazz.getAnnotation(Registry.class) != null;
-    }
+  }
+
+  /**
+   * 判断该类是否加了Registry注解
+   */
+  private boolean isRegistry(Class<?> clazz) {
+    return clazz.getAnnotation(Registry.class) != null;
+  }
 
 }
